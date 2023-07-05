789a8ee9bd82034ecfbac5820408ea10af73f62f
Jayapand94 <jayapandiyan4034@gail.com>
2023-07-05 16:03:54 +0530

    first commit

---
+++ src/com/github/org/LoginPage.java
@@ -0,0 +1,23 @@
+package com.github.org;
+
+public class LoginPage {
+	
+	public static void main(String[] args) {
+		String s="Automation";
+		String r=" ";
+		char c;
+		for (int i = s.length()-1; i >=0; i--) {
+			
+			c=s.charAt(i);
+			
+			r=r+c;
+			
+			
+		}
+		System.out.println(r);
+		
+		
+	}
+	}
+
+
